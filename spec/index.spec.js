279900829dfd7cc8a4bf78f7b3426af69634ad70
zhoujiang <zhoujiang@zhaozhi.me>
2018-04-25 19:56:23 +0800

    add test

---
+++ spec/index.spec.js
@@ -0,0 +1,4 @@
+describe suffix for img _link_script works
+describe prefix for img _link_script works
+describe pattern for img _link_script works
+describe no other changes for img _link_script works
